778f6c3d5949f53a1e4a14aeda97ef5eaacac448
Adam Shumann <ashumann13@gmail.com>
2018-01-30 08:45:26 -0600

    Revert standard-autoformat

--- index.js
+++ index.js
@@ -6,139 +6,141 @@
  * Licensed under the MIT license.
  */
 
-'use strict'
+'use strict';
 
 var through = require('through2'),
-  PluginError = require('plugin-error'),
-  log = require('fancy-log'),
-  http = require('http'),
-  https = require('https'),
-  util = require('util'),
-  path = require('path'),
-  fs = require('fs')
+    PluginError = require('plugin-error'),
+    log = require('fancy-log'),
+    http = require('http'),
+    https = require('https'),
+    util = require('util'),
+    path = require('path'),
+    fs = require('fs');
 
-var PLUGIN_NAME = 'gulp-screeps'
+var PLUGIN_NAME = 'gulp-screeps';
 
 module.exports = function (opt) {
-  opt = opt || {}
+    opt = opt || {};
 
-  var files = []
-  var modules = {}
+    var files = [];
+    var modules = {};
 
-  if (typeof opt.host !== 'string') {
-    opt.host = 'screeps.com'
-  }
+    if (typeof opt.host !== 'string'){
+        opt.host = 'screeps.com';
+    }
+    
+    if (typeof opt.port !== 'number'){
+        opt.port = 443;
+    }
 
-  if (typeof opt.port !== 'number') {
-    opt.port = 443
-  }
+    if (typeof opt.secure !== 'boolean'){
+        opt.secure = opt.port === 443;
+    }
 
-  if (typeof opt.secure !== 'boolean') {
-    opt.secure = opt.port === 443
-  }
+    if ((typeof opt.email !== 'string' || typeof opt.password !== 'string') && typeof opt.token !== 'string') {
+        throw new PluginError(PLUGIN_NAME, 'Please provide account information');
+    }
 
-  if ((typeof opt.email !== 'string' || typeof opt.password !== 'string') && typeof opt.token !== 'string') {
-    throw new PluginError(PLUGIN_NAME, 'Please provide account information')
-  }
+    if (typeof opt.branch !== 'string'){
+        opt.branch = 'default';
+    }
 
-  if (typeof opt.branch !== 'string') {
-    opt.branch = 'default'
-  }
-
-  function bufferContents (file, enc, cb) {
+    function bufferContents(file, enc, cb) {
         // ignore empty files
-    if (file.isNull()) {
-      cb()
-      return
+        if (file.isNull()) {
+            cb();
+            return;
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            cb();
+            return;
+        }
+        files.push(file);
+
+        cb();
     }
 
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'))
-      cb()
-      return
-    }
-    files.push(file)
+    function endStream(cb) {
 
-    cb()
-  }
+        files.map(function (file) {
+            var name = path.basename(file.path).replace(/\.js$/,'');
+            modules[name] = file.contents.toString('utf-8');
+        });
 
-  function endStream (cb) {
-    files.map(function (file) {
-      var name = path.basename(file.path).replace(/\.js$/, '')
-      modules[name] = file.contents.toString('utf-8')
-    })
+        var request = (opt.secure ? https : http).request;
+        var reqOpts = {
+            hostname: opt.host,
+            port: opt.port,
+            path: opt.ptr ? '/ptr/api/user/code' : '/api/user/code',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        };
+        
+        if(opt.token) {
+            reqOpts.headers['x-token'] = opt.token;
+        } else if(opt.email && opt.password) {
+            reqOpts.auth = opt.email + ':' + opt.password;
+        }
 
-    var request = (opt.secure ? https : http).request
-    var reqOpts = {
-      hostname: opt.host,
-      port: opt.port,
-      path: opt.ptr ? '/ptr/api/user/code' : '/api/user/code',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    }
-
-    if (opt.token) {
-      reqOpts.headers['x-token'] = opt.token
-    } else if (opt.email && opt.password) {
-      reqOpts.auth = opt.email + ':' + opt.password
-    }
-
-    var req = request(reqOpts,
+        var req = request(reqOpts,
             function (res) {
-              res.setEncoding('utf8')
+                res.setEncoding('utf8');
 
-              var data = ''
+                var data = '';
 
-              res.on('data', function (chunk) {
-                data += chunk
-              })
+                res.on('data', function (chunk) {
+                    data += chunk;
+                });
 
-              res.on('end', function () {
-                try {
-                  data = JSON.parse(data)
-                } catch (e) {}
-                if (data.ok) {
-                  var msg = 'Committed to Screeps account'
-                  if (opt.email) {
-                    msg += ' "' + opt.email + '"'
-                  }
-                  if (opt.token) {
-                    msg += ' with token'
-                  }
-                  if (opt.branch) {
-                    msg += ' branch "' + opt.branch + '"'
-                  }
-                  if (opt.host !== 'screeps.com') {
-                    msg += ' on server "' + opt.host + '"'
-                  }
-                  msg += '.'
-                  if (res.headers['x-ratelimit-limit']) {
-                    let limit = res.headers['x-ratelimit-limit']
-                    let remaining = res.headers['x-ratelimit-remaining']
-                    let reset = res.headers['x-ratelimit-reset']
-                    msg += `  RateLimiting: (${remaining}/${limit} ${(parseInt(reset) * 1000) - Date.now()}ms to reset)`
-                  }
-                  log(msg)
-                  cb()
-                } else {
-                  log('Error while committing to Screeps: ' + util.inspect(data))
-                  cb('Error while committing to Screeps: ' + util.inspect(data))
-                }
-              })
+                res.on('end', function () {
+                    try {
+                        data = JSON.parse(data);
+                    } catch(e) {}
+                    if (data.ok) {
+                        var msg = 'Committed to Screeps account';
+                        if (opt.email) {
+                            msg += ' "' + opt.email + '"';
+                        }
+                        if (opt.token) {
+                            msg += ' with token';
+                        }
+                        if (opt.branch) {
+                            msg += ' branch "' + opt.branch + '"';
+                        }
+                        if (opt.host !== "screeps.com") {
+                            msg += ' on server "' + opt.host + '"';
+                        }
+                        msg += '.';
+                        if (res.headers['x-ratelimit-limit']) {
+                            var limit = res.headers['x-ratelimit-limit'];
+                            var remaining = res.headers['x-ratelimit-remaining'];
+                            var reset = res.headers['x-ratelimit-reset'];
+                            msg += '  RateLimiting: (' + remaining + '/' + limit + ' ' + (parseInt(reset) * 1000) - Date.now()} + 'ms to reset)';
+                        }
+                        log(msg);
+                        cb();
+                    }
+                    else {
+                        log('Error while committing to Screeps: ' + util.inspect(data));
+                        cb('Error while committing to Screeps: ' + util.inspect(data));
+                    }
+                });
 
-              res.on('error', cb)
-            })
+                res.on('error', cb);
+            });
 
-    var data = {
-      branch: opt.branch,
-      modules: modules
+        var data = {
+            branch: opt.branch,
+            modules: modules
+        };
+
+        req.write(JSON.stringify(data));
+        req.end();
     }
 
-    req.write(JSON.stringify(data))
-    req.end()
-  }
-
-  return through.obj(bufferContents, endStream)
-}
+    return through.obj(bufferContents, endStream);
+};
